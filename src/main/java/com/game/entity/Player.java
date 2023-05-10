92b1a578e968e0438c7354e06d755884d3f5d5b0
UBCh <u2b127@gmail.com>
2023-05-10 20:51:07 +0300

    the task has been implemented before clause 11-12

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -15,6 +15,7 @@ import java.util.Date;
 public class Player {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
     private Long id;
 
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -3,6 +3,7 @@ package com.game.repository;
 import com.game.entity.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.query.NativeQuery;
@@ -53,22 +54,36 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @Override
     public Player save(Player player) {
-        return null;
+        try(Session session=sessionFactory.openSession()){
+            Transaction transaction=session.beginTransaction();
+            session.save(player);
+            transaction.commit();
+            return player;}
     }
 
     @Override
     public Player update(Player player) {
-        return null;
+        try(Session session=sessionFactory.openSession()){
+            Transaction transaction=session.beginTransaction();
+            session.update(player);
+            transaction.commit();
+        return player;}
     }
 
     @Override
     public Optional<Player> findById(long id) {
-        return Optional.empty();
+        try(Session session=sessionFactory.openSession()){
+            Player player = session.find(Player.class, id);
+            return Optional.of(player);}
     }
 
     @Override
     public void delete(Player player) {
-
+        try(Session session=sessionFactory.openSession()){
+            Transaction transaction=session.beginTransaction();
+            session.delete(player);
+            transaction.commit();
+          }
     }
 
     @PreDestroy
